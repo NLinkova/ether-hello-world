619a0ad28331df3a17997ec261b9cea96f1dbb0e
Natalia Linkova <linkova1109@yandex.ru>
2022-08-26 12:05:15 +1000

    deploy and interact message

---
+++ scripts/deploy.js
@@ -0,0 +1,12 @@
+async function main() {
+  const HelloWorld = await ethers.getContractFactory("HelloWorld");
+  const hello_world = await HelloWorld.deploy("Hello message");
+  console.log("Contract was deployed to address: ", hello_world.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
